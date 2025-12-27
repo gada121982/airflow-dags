b3769885da99f90809c6a1fc92c1fc4f831e8c0e
Harry Nguyen <gada121982@gmail.com>
2025-12-27 17:11:27 +0700

    test

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ from datetime import datetime
  
 from airflow.sdk import DAG, task
 from airflow.providers.standard.operators.bash import BashOperator
+
  
 # A Dag represents a workflow, a collection of tasks
 with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
@@ -17,4 +18,4 @@ with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") a
         print("world")
  
     # Set dependencies between tasks
-    hello >> airflow() >> world()
+    hello >> [airflow(), world()]
